8a8a08c9db053ab3668598abb0a70290b5ce99cf
G Ajeet <55391283+gajeet11000@users.noreply.github.com>
2026-08-14 12:40:59 +0530

    added enabled lsp servers

--- dotfiles/nvim/.config/nvim/lua/servers/init.lua
+++ dotfiles/nvim/.config/nvim/lua/servers/init.lua
@@ -1,3 +1,21 @@
 return {
   basedpyright = require("servers.basedpyright"),
+  lua_ls = {},
+  jdtls = {},
+  ruff = {},
+
+  ts_ls = {},
+  eslint = {},
+  html = {},
+  cssls = {},
+  emmet_language_server = {},
+
+  jsonls = {},
+  bashls = {},
+  yamlls = {},
+
+  dockerls = {},
+  docker_compose_language_service = {},
+
+  marksman = {},
 }
